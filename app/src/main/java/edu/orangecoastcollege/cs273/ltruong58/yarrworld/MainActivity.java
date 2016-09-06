84a63ddb20e4a3fca6b1b2a494faa3393c7ecc78
ltruong58 <long.truongnguyenthanh@gmail.com>
2016-09-06 16:12:57 -0700

    final version

--- app/src/main/java/edu/orangecoastcollege/cs273/ltruong58/yarrworld/MainActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/ltruong58/yarrworld/MainActivity.java
@@ -1,27 +1,39 @@
 package edu.orangecoastcollege.cs273.ltruong58.yarrworld;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Toast;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
 
     //Create button
     private Button speakButton;
+    private Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        //Restoring user settings from a previous session
         super.onCreate(savedInstanceState);
+
+        //Inflating (!) opening the layout called activity_main
         setContentView(R.layout.activity_main);
 
-        speakButton = (Button) findViewById(R.id.speak_button);
+        // Hook up the speakButton to the one in our view
+        speakButton = (Button) findViewById(R.id.speakButton);
+
+        // Wire up the speakButton to perform an onClick event
         speakButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public  void onClick(View v) {
-                Toast.makeText(MainActivity.this, R.string.speak_text, Toast.LENGTH_SHORT).show();
+                // Use the static reference to "Toast" to display the message:
+                // Toast = vanishes after set amount of time
+                // Dialog = requires user input
+                Toast.makeText(context, R.string.toast_text, Toast.LENGTH_LONG).show();
             }
         });
 
