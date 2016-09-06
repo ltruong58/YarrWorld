4b979e29c28b88db3e7578a94ace794b1a29efdc
Long Truong <long.truongnguyenthanh@gmail.com>
2016-09-06 10:05:18 -0700

    version 2

--- app/src/main/java/edu/orangecoastcollege/cs273/ltruong58/yarrworld/MainActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/ltruong58/yarrworld/MainActivity.java
@@ -2,12 +2,28 @@ package edu.orangecoastcollege.cs273.ltruong58.yarrworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    //Create button
+    private Button speakButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        speakButton = (Button) findViewById(R.id.speak_button);
+        speakButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public  void onClick(View v) {
+                Toast.makeText(MainActivity.this, R.string.speak_text, Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
